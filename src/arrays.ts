3e7b55160dde1bd881fadffd554ee78cdcfa50d8
Ryan Giles <ryan.giles@mymail.champlain.edu>
2023-09-11 20:29:41 -0400

    Finished bookEndList, tripleNumbers, and stringsToIntegers functions

--- src/arrays.ts
+++ src/arrays.ts
@@ -5,7 +5,14 @@
  * the number twice.
  */
 export function bookEndList(numbers: number[]): number[] {
-    return numbers;
+    const length: number = numbers.length;
+    const first_last: number[] = [];
+    if (length === 0) {
+        return first_last;
+    }
+    first_last[0] = numbers[0];
+    first_last[1] = numbers[length - 1];
+    return first_last;
 }
 
 /**
@@ -13,7 +20,8 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    const tripled: number[] = numbers.map((num: number): number => num * 3);
+    return tripled;
 }
 
 /**
@@ -21,7 +29,10 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
-    return [];
+    const integers = numbers.map((num: string): number =>
+        Number.parseInt(num) ? +num : 0
+    );
+    return integers;
 }
 
 /**
