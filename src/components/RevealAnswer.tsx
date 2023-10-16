ee9f75ceb27d995f2cc7b78d54f86b3b2f516e1a
rgiles4 <ryan.giles@maine.edu>
2023-10-16 14:03:06 -0400

    Completed tests for RevealAnswer Component

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,23 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): JSX.Element {
-    return <div>Reveal Answer</div>;
+    /* 
+    You will need a state to handle whether the text is visible.
+    There is a button labelled Reveal Answer that inverts the state.
+    The text 42 is not initially visible.
+    When the button is clicked, the text 42 should be visible. 
+    */
+
+    const [visible, setVisible] = useState<boolean>(false);
+
+    function flipVisibility(): void {
+        setVisible(!visible);
+    }
+
+    return (
+        <div>
+            <Button onClick={flipVisibility}>Show/Hide</Button>
+            {visible && <div>42</div>}
+        </div>
+    );
 }
